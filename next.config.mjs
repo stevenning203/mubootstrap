0e91b998281e00c0a1aaf1eafbdf83b4f06848fb
stevenning203 <stevenning.203@gmail.com>
2023-03-26 16:29:22 -0700

    fix #2

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,7 @@
    */
 const nextConfig = {
   images: { unoptimized: true },
-  experimental: { images: { unoptimized: true } },
-  basePath: "/mubootstrap",
+  experimental: { images: { unoptimized: true } }
 };
 
 export default nextConfig;
