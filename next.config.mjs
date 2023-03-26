9c8ee0ef27da5502ecb6cae7e581c7c539718996
stevenning203 <stevenning.203@gmail.com>
2023-03-26 16:28:03 -0700

    Fix build

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const nextConfig = {
   images: { unoptimized: true },
   experimental: { images: { unoptimized: true } },
   basePath: "/mubootstrap",
-
-  assetPrefix: (process.env.NODE_ENV === 'production') ? './' : undefined,
-  output: 'export',
-  distDir: "out",
 };
 
 export default nextConfig;
